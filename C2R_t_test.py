d44b5408d87b4dda1467bfd4d60d4a54173d657c
myabiabi <abigail.montante.arenas@gmail.com>
2025-06-30 12:08:08 -0600

    ensayo funcional

---
+++ C2R_t_test.py
@@ -0,0 +1,76 @@
+import cometspy as c
+import cobra.io
+import matplotlib.pyplot as plt
+
+#############
+test_tube = c.layout()
+test_tube.set_specific_metabolite("h2o_e", 100)
+test_tube.set_specific_metabolite("o2_e", 10)
+test_tube.set_specific_metabolite("pi_e", 1)
+test_tube.set_specific_metabolite("glu__L_e", 0.01)
+test_tube.set_specific_metabolite("gly_e", 0.01)
+test_tube.set_specific_metabolite("zn2_e", 1)
+test_tube.set_specific_metabolite("ala__L_e", 0.01)
+test_tube.set_specific_metabolite("lys_L_e", 0.01)
+test_tube.set_specific_metabolite("asp_L_e", 0.01)
+
+# create the model using CobraPy functionality
+ruta_C2R = '/home/abigaylmontantearenas/Documents/practicas/MODELOS/data/GEM/cvm_C2R.xml'
+C2R_cobra = cobra.io.read_sbml_model(ruta_C2R)
+
+# use the loaded model to build a comets model
+C2R = c.model(C2R_cobra)
+
+# remove the bounds from glucose import (will be set dynamically by COMETS)
+C2R.change_bounds('EX_glc__D_e', -1000, 1000)
+
+# set the model's initial biomass
+C2R.initial_pop = [0, 0, 5e-6]
+
+# add it to the test_tube
+test_tube.add_model(C2R)
+
+# Set the parameters that are different from the default
+sim_params = c.params()
+sim_params.set_param('defaultVmax', 18.5)
+sim_params.set_param('defaultKm', 0.000015)
+sim_params.set_param('maxCycles', 1000)
+sim_params.set_param('timeStep', 0.01)
+sim_params.set_param('spaceWidth', 1)
+sim_params.set_param('maxSpaceBiomass', 10)
+sim_params.set_param('minSpaceBiomass', 1e-11)
+sim_params.set_param('writeMediaLog', True)
+
+experiment = c.comets(test_tube, sim_params)
+experiment.run()
+
+#############
+import os
+
+# Crear la carpeta si no existe
+output_folder = 'graficas'
+os.makedirs(output_folder, exist_ok=True)
+
+ax = experiment.total_biomass.plot(x = 'cycle')
+ax.set_ylabel("Biomass (gr.)")
+
+media = experiment.media.copy()
+media = media[media.conc_mmol<900]
+
+fig, ax = plt.subplots()
+media.groupby('metabolite').plot(x='cycle', ax =ax, y='conc_mmol')
+ax.legend(('acetate','ethanol', 'formate', 'glucose'))
+ax.set_ylabel("Concentration (mmol)")
+
+# Guardar figura en la carpeta
+output_path = os.path.join(output_folder, '063025_TTEST_C2R_nutrientes_LB10.png')
+plt.savefig(output_path)
+
+# Mostrar figura
+plt.show()
+
+
+##pruebaaas
+
+
+
