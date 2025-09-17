00e5b49808fd7e718f8100a353dac99c46920286
myabiabi <abigail.montante.arenas@gmail.com>
2025-09-16 19:25:02 -0600

    modified:   ST00164.py 	deleted:    ST00164_NW.py 	deleted:    ST0164_GPS.py

--- ST00164.py
+++ ST00164.py
@@ -320,8 +320,9 @@ for m in present:
     print("-", m)
 
 
-#############
-#080725
+# =================
+# 080725
+# =================
 
 ST164_model = cobra.io.read_sbml_model('/home/abigaylmontantearenas/Documents/practicas/MODELOS/data/carveme_models/ST00164.xml')
 ST164_model.medium
@@ -462,8 +463,9 @@ if ex_prbamp is None:
 test_tube.add_model(ST164_model)
 test_tube.set_specific_metabolite("prbamp_e", 10)
 
-
-#090325
+# =================
+#  090325 
+# =================
 
 ST00164 = cobra.io.read_sbml_model('/home/abigaylmontantearenas/Documents/practicas/MODELOS/data/carveme_models/carve164.xml')
 ST164_dimon = cobra.io.read_sbml_model('/home/abigaylmontantearenas/Documents/practicas/MODELOS/data/ST00164_diamon.xml')
@@ -566,7 +568,9 @@ output_folder = 'graficas/CARVEME/DIMONT'
 output_path = os.path.join(output_folder, 'ST000164_diamont.png')
 plt.savefig(output_path)
 
-# 090925
+# =================
+#  090925
+# =================
 import cometspy as c
 import cobra.io
 import pandas as pd
@@ -664,4 +668,216 @@ ax.set_ylabel("Biomass (gr.)")
 
 output_folder = 'graficas/CARVEME/DIMONT'
 output_path = os.path.join(output_folder, '164.png')
+plt.savefig(output_path)
+
+# =================
+#  160925
+# =================
+#usando el GEM con gapfiling de carveme _NW
+
+#Start by loading required packages, including the COMETS toolbox
+import cometspy as c
+import cobra.io
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+
+ST164 = c.model(cobra.io.read_sbml_model('/home/abigaylmontantearenas/Documents/practicas/MODELOS/data/carveme_models/new_ST00164.xml'))
+ST164.id = 'ST00164'
+
+
+# set its initial biomass, 5e-6 gr at coordinate [0,0]
+ST164.initial_pop = [0, 0, 5e-8]
+
+# create an empty layout
+test_tube = c.layout()
+
+# add the models to the test tube
+test_tube.add_model(ST164)
+
+
+test_tube.set_specific_metabolite("h2o_e", 100)
+test_tube.set_specific_metabolite("o2_e", 10)
+test_tube.set_specific_metabolite("pi_e", 10)
+test_tube.set_specific_metabolite("prbamp_e", 10)
+test_tube.set_specific_metabolite("glu__L_e", 0.1)
+test_tube.set_specific_metabolite("mn2_e", 10)
+test_tube.set_specific_metabolite("gly_e", 0.1)
+test_tube.set_specific_metabolite("zn2_e", 10)
+test_tube.set_specific_metabolite("ala__L_e", 0.1)
+test_tube.set_specific_metabolite("lys__L_e", 0.1)
+test_tube.set_specific_metabolite("asp__L_e", 0.1)
+test_tube.set_specific_metabolite("so4_e", 0.1)
+test_tube.set_specific_metabolite("arg__L_e", 0.1)
+test_tube.set_specific_metabolite("ser__L_e", 0.1)
+test_tube.set_specific_metabolite("cu2_e", 0.1)
+test_tube.set_specific_metabolite("met__L_e", 0.1)
+test_tube.set_specific_metabolite("trp__L_e", 0.1)
+test_tube.set_specific_metabolite("phe__L_e", 0.1)
+test_tube.set_specific_metabolite("h_e", 0.1)
+test_tube.set_specific_metabolite("tyr__L_e", 0.1)
+test_tube.set_specific_metabolite("cys__L_e", 0.1)
+test_tube.set_specific_metabolite("ura_e", 0.1)
+test_tube.set_specific_metabolite("cl_e", 0.1)
+test_tube.set_specific_metabolite("leu__L_e", 0.1) 
+test_tube.set_specific_metabolite("his__L_e", 0.1)
+test_tube.set_specific_metabolite("pro__L_e", 0.1)
+test_tube.set_specific_metabolite("cobalt2_e", 10)
+test_tube.set_specific_metabolite("val__L_e", 0.1)
+test_tube.set_specific_metabolite("thr__L_e", 0.1)
+test_tube.set_specific_metabolite("adn_e", 0.01)
+test_tube.set_specific_metabolite("thymd_e", 0.01)
+test_tube.set_specific_metabolite("k_e", 10)
+test_tube.set_specific_metabolite("h2s_e", 0.01)
+test_tube.set_specific_metabolite("ins_e", 0.01)
+test_tube.set_specific_metabolite("uri_e", 0.01)
+test_tube.set_specific_metabolite("mg2_e", 10)
+test_tube.set_specific_metabolite("gsn_e", 0.01)
+test_tube.set_specific_metabolite("ile__L_e", 0.1)
+test_tube.set_specific_metabolite("cys__L_e", 0.1)
+test_tube.set_specific_metabolite("skm_e", 0.01)
+test_tube.set_specific_metabolite("fol_e", 0.01)
+test_tube.set_specific_metabolite("dadn_e", 0.01)
+test_tube.set_specific_metabolite("lipoate_e", 0.01)
+test_tube.set_specific_metabolite("na1_e", 10)
+test_tube.set_specific_metabolite("cd2_e", 10)
+test_tube.set_specific_metabolite("aso4_e", 10)
+test_tube.set_specific_metabolite("fe2_e", 10)
+test_tube.set_specific_metabolite("fe3_e", 10)
+test_tube.set_specific_metabolite("cro4_e", 10)
+test_tube.set_specific_metabolite("nh3_c_e", 10)
+test_tube.set_specific_metabolite("pheme_e", 10)
+test_tube.set_specific_metabolite("cmp_e", 10)
+test_tube.set_specific_metabolite("ump_e", 10)
+test_tube.set_specific_metabolite("gmp_e", 10)
+test_tube.set_specific_metabolite("pydx_e", 10)
+test_tube.set_specific_metabolite("nac_e", 10)
+test_tube.set_specific_metabolite("ribflv_e", 10)
+test_tube.set_specific_metabolite("pphn_e", 10)
+test_tube.set_specific_metabolite("hxan_e", 10)
+test_tube.set_specific_metabolite("thmpp_e", 0.01)
+test_tube.set_specific_metabolite("cbl1_e", 0.01)
+
+# Add typical trace metabolites and oxygen coli as static
+trace_metabolites = ['ca2_e', 'cl_e', 'cobalt2_e', 'cu2_e', 'fe2_e', 'fe3_e', 'h_e', 'k_e', 'h2o_e', 'mg2_e',
+                     'mn2_e', 'mobd_e', 'na1_e', 'ni2_e', 'nh4_e', 'o2_e', 'pi_e', 'so4_e', 'zn2_e']
+
+for i in trace_metabolites:
+    test_tube.set_specific_metabolite(i, 1000)
+    test_tube.set_specific_static(i, 1000)
+
+sim_params = c.params()
+experiment = c.comets(test_tube, sim_params)
+experiment.run()
+
+ax = experiment.total_biomass.plot(x = 'cycle', color = 'orange')
+ax.set_ylabel("Biomass (gr.)")
+
+output_folder = 'graficas_/CARVEME'
+output_path = os.path.join(output_folder, 'ST00164_new_.png')
+plt.savefig(output_path)
+
+#ahora usando el GEM de gapseq 
+#Start by loading required packages, including the COMETS toolbox
+import cometspy as c
+import cobra.io
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+
+ST164 = c.model(cobra.io.read_sbml_model('/home/abigaylmontantearenas/Documents/practicas/MODELOS/data/gapseq_models/bac60.xml'))
+ST164.id = 'ST00164'
+
+
+# set its initial biomass, 5e-6 gr at coordinate [0,0]
+ST164.initial_pop = [0, 0, 5e-8]
+
+# create an empty layout
+test_tube = c.layout()
+
+# add the models to the test tube
+test_tube.add_model(ST164)
+
+
+test_tube.set_specific_metabolite("h2o_e", 100)
+test_tube.set_specific_metabolite("o2_e", 10)
+test_tube.set_specific_metabolite("pi_e", 10)
+test_tube.set_specific_metabolite("prbamp_e", 10)
+test_tube.set_specific_metabolite("glu__L_e", 0.1)
+test_tube.set_specific_metabolite("mn2_e", 10)
+test_tube.set_specific_metabolite("gly_e", 0.1)
+test_tube.set_specific_metabolite("zn2_e", 10)
+test_tube.set_specific_metabolite("ala__L_e", 0.1)
+test_tube.set_specific_metabolite("lys__L_e", 0.1)
+test_tube.set_specific_metabolite("asp__L_e", 0.1)
+test_tube.set_specific_metabolite("so4_e", 0.1)
+test_tube.set_specific_metabolite("arg__L_e", 0.1)
+test_tube.set_specific_metabolite("ser__L_e", 0.1)
+test_tube.set_specific_metabolite("cu2_e", 0.1)
+test_tube.set_specific_metabolite("met__L_e", 0.1)
+test_tube.set_specific_metabolite("trp__L_e", 0.1)
+test_tube.set_specific_metabolite("phe__L_e", 0.1)
+test_tube.set_specific_metabolite("h_e", 0.1)
+test_tube.set_specific_metabolite("tyr__L_e", 0.1)
+test_tube.set_specific_metabolite("cys__L_e", 0.1)
+test_tube.set_specific_metabolite("ura_e", 0.1)
+test_tube.set_specific_metabolite("cl_e", 0.1)
+test_tube.set_specific_metabolite("leu__L_e", 0.1) 
+test_tube.set_specific_metabolite("his__L_e", 0.1)
+test_tube.set_specific_metabolite("pro__L_e", 0.1)
+test_tube.set_specific_metabolite("cobalt2_e", 10)
+test_tube.set_specific_metabolite("val__L_e", 0.1)
+test_tube.set_specific_metabolite("thr__L_e", 0.1)
+test_tube.set_specific_metabolite("adn_e", 0.01)
+test_tube.set_specific_metabolite("thymd_e", 0.01)
+test_tube.set_specific_metabolite("k_e", 10)
+test_tube.set_specific_metabolite("h2s_e", 0.01)
+test_tube.set_specific_metabolite("ins_e", 0.01)
+test_tube.set_specific_metabolite("uri_e", 0.01)
+test_tube.set_specific_metabolite("mg2_e", 10)
+test_tube.set_specific_metabolite("gsn_e", 0.01)
+test_tube.set_specific_metabolite("ile__L_e", 0.1)
+test_tube.set_specific_metabolite("cys__L_e", 0.1)
+test_tube.set_specific_metabolite("skm_e", 0.01)
+test_tube.set_specific_metabolite("fol_e", 0.01)
+test_tube.set_specific_metabolite("dadn_e", 0.01)
+test_tube.set_specific_metabolite("lipoate_e", 0.01)
+test_tube.set_specific_metabolite("na1_e", 10)
+test_tube.set_specific_metabolite("cd2_e", 10)
+test_tube.set_specific_metabolite("aso4_e", 10)
+test_tube.set_specific_metabolite("fe2_e", 10)
+test_tube.set_specific_metabolite("fe3_e", 10)
+test_tube.set_specific_metabolite("cro4_e", 10)
+test_tube.set_specific_metabolite("nh3_c_e", 10)
+test_tube.set_specific_metabolite("pheme_e", 10)
+test_tube.set_specific_metabolite("cmp_e", 10)
+test_tube.set_specific_metabolite("ump_e", 10)
+test_tube.set_specific_metabolite("gmp_e", 10)
+test_tube.set_specific_metabolite("pydx_e", 10)
+test_tube.set_specific_metabolite("nac_e", 10)
+test_tube.set_specific_metabolite("ribflv_e", 10)
+test_tube.set_specific_metabolite("pphn_e", 10)
+test_tube.set_specific_metabolite("hxan_e", 10)
+test_tube.set_specific_metabolite("thmpp_e", 0.01)
+test_tube.set_specific_metabolite("cbl1_e", 0.01)
+
+# Add typical trace metabolites and oxygen coli as static
+trace_metabolites = ['ca2_e', 'cl_e', 'cobalt2_e', 'cu2_e', 'fe2_e', 'fe3_e', 'h_e', 'k_e', 'h2o_e', 'mg2_e',
+                     'mn2_e', 'mobd_e', 'na1_e', 'ni2_e', 'nh4_e', 'o2_e', 'pi_e', 'so4_e', 'zn2_e']
+
+for i in trace_metabolites:
+    test_tube.set_specific_metabolite(i, 1000)
+    test_tube.set_specific_static(i, 1000)
+
+sim_params = c.params()
+experiment = c.comets(test_tube, sim_params)
+experiment.run()
+
+ax = experiment.total_biomass.plot(x = 'cycle', color = 'orange')
+ax.set_ylabel("Biomass (gr.)")
+
+output_folder = 'graficas_/GapSeq'
+output_path = os.path.join(output_folder, 'ST00042_.png')
 plt.savefig(output_path)
