97e504323c4e481d661b85c5bbe30821d5d95223
myabiabi <abigail.montante.arenas@gmail.com>
2025-09-29 10:25:45 -0600

    new file:   .current_global_0x7f47e55546d0 	new file:   .current_global_0x7fe0e009b650 	new file:   .current_layout_0x7f47e55546d0 	new file:   .current_layout_0x7fe0e009b650 	new file:   .current_package_0x7f47e55546d0 	new file:   .current_package_0x7fe0e009b650 	new file:   .current_script_0x7f47e55546d0 	new file:   .current_script_0x7fe0e009b650 	new file:   03_graficas/AHhahah.png 	new file:   42x143x164.py 	new file:   ST00000.py 	new file:   ahhhH.py 	new file:   comets_error_log.txt 	modified:   competencia_143x164.py 	modified:   ecoli.py 	new file:   intentomil.py 	new file:   intentomillon.py 	new file:   totalbiomasslog_0x7f47e55546d0 	new file:   totalbiomasslog_0x7fe0e009b650

--- competencia_143x164.py
+++ competencia_143x164.py
@@ -1,106 +1,3 @@
-# Start by loading required packages, including the COMETS toolbox
-import cometspy as c
-import cobra.io
-import pandas as pd
-import matplotlib.pyplot as plt
-import os
-
-ST00143 = cobra.io.read_sbml_model('/home/abigaylmontantearenas/Documents/practicas/MODELOS/01_data/models_carveme/DIAMONT_2/ST00143dd.xml')
-ST00164 = cobra.io.read_sbml_model('/home/abigaylmontantearenas/Documents/practicas/MODELOS/01_data/models_carveme/DIAMONT_2/ST00164dd.xml')
-
-st143 = c.model(ST00143)
-st143.id = 'Paenibacillus'
-st164 = c.model(ST00164)
-st164.id = 'Bacillus'
-
-# set its initial biomass, 5e-6 gr at coordinate [0,0]
-st143.initial_pop = [0, 0, 5e-8]
-st164.initial_pop = [0, 0, 5e-8]
-
-# create an empty layout
-test_tube = c.layout()
-
-# add the models to the test tube
-test_tube.add_model(st143)
-test_tube.add_model(st164)
-
-test_tube.set_specific_metabolite("h2o_e", 100)
-test_tube.set_specific_metabolite("o2_e", 10)
-test_tube.set_specific_metabolite("pi_e", 10)
-test_tube.set_specific_metabolite("prbamp_e", 10)
-test_tube.set_specific_metabolite("glu__L_e", 0.1)
-test_tube.set_specific_metabolite("mg2_e", 0.1)
-test_tube.set_specific_metabolite("gly_e", 0.1)
-test_tube.set_specific_metabolite("zn2_e", 10)
-test_tube.set_specific_metabolite("ala__L_e", 0.1)
-test_tube.set_specific_metabolite("lys__L_e", 0.1)
-test_tube.set_specific_metabolite("asp__L_e", 0.1)
-test_tube.set_specific_metabolite("so4_e", 0.1)
-test_tube.set_specific_metabolite("arg__L_e", 0.1)
-test_tube.set_specific_metabolite("ser__L_e", 0.1)
-test_tube.set_specific_metabolite("cu2_e", 0.1)
-test_tube.set_specific_metabolite("met__L_e", 0.1)
-test_tube.set_specific_metabolite("trp__L_e", 0.1)
-test_tube.set_specific_metabolite("phe__L_e", 0.1)
-test_tube.set_specific_metabolite("h_e", 0.1)
-test_tube.set_specific_metabolite("tyr__L_e", 0.1)
-test_tube.set_specific_metabolite("cys__L_e", 0.1)
-test_tube.set_specific_metabolite("ura_e", 0.1)
-test_tube.set_specific_metabolite("cl_e", 0.1)
-test_tube.set_specific_metabolite("leu__L_e", 0.1) 
-test_tube.set_specific_metabolite("his__L_e", 0.1)
-test_tube.set_specific_metabolite("pro__L_e", 0.1)
-test_tube.set_specific_metabolite("cobalt2_e", 10)
-test_tube.set_specific_metabolite("val__L_e", 0.1)
-test_tube.set_specific_metabolite("thr__L_e", 0.1)
-test_tube.set_specific_metabolite("adn_e", 0.01)
-test_tube.set_specific_metabolite("thymd_e", 0.01)
-test_tube.set_specific_metabolite("k_e", 10)
-test_tube.set_specific_metabolite("h2s_e", 0.01)
-test_tube.set_specific_metabolite("ins_e", 0.01)
-test_tube.set_specific_metabolite("uri_e", 0.01)
-test_tube.set_specific_metabolite("mg2_e", 10)
-test_tube.set_specific_metabolite("gsn_e", 0.01)
-test_tube.set_specific_metabolite("ile__L_e", 0.1)
-test_tube.set_specific_metabolite("cys__L_e", 0.1)
-test_tube.set_specific_metabolite("skm_e", 0.01)
-test_tube.set_specific_metabolite("fol_e", 0.01)
-test_tube.set_specific_metabolite("dadn_e", 0.01)
-test_tube.set_specific_metabolite("lipoate_e", 0.01)
-test_tube.set_specific_metabolite("na1_e", 10)
-test_tube.set_specific_metabolite("cd2_e", 10)
-test_tube.set_specific_metabolite("aso4_e", 10)
-test_tube.set_specific_metabolite("fe2_e", 10)
-test_tube.set_specific_metabolite("fe3_e", 10)
-test_tube.set_specific_metabolite("cro4_e", 10)
-test_tube.set_specific_metabolite("nh3_c", 10)
-test_tube.set_specific_metabolite("pheme", 10)
-test_tube.set_specific_metabolite("cmp", 10)
-test_tube.set_specific_metabolite("ump", 10)
-test_tube.set_specific_metabolite("gmp", 10)
-test_tube.set_specific_metabolite("pydx", 10)
-test_tube.set_specific_metabolite("nac", 10)
-test_tube.set_specific_metabolite("ribflv", 10)
-test_tube.set_specific_metabolite("pphn", 10)
-test_tube.set_specific_metabolite("hxan", 10)
-
-comp_params = c.params()
-comp_params.set_param('maxCycles', 240)
-
-comp_assay = c.comets(test_tube, comp_params)
-comp_assay.run()
-
-biomass = comp_assay.total_biomass
-biomass['t'] = biomass['cycle'] * comp_assay.parameters.all_params['timeStep']
-
-myplot = biomass.drop(columns=['cycle']).plot(x = 't')
-myplot.set_ylabel("Biomass (gr.)")
-
-output_folder = '03_graficas/CARVEME/competencia'
-output_path = os.path.join(output_folder, 'competencia_143x164.png')
-plt.savefig(output_path)
-
-
 # 092425
 
 import cometspy as c
@@ -110,8 +7,8 @@ import matplotlib.pyplot as plt
 import os
 
 ST143_d = c.model(cobra.io.read_sbml_model('/home/abigaylmontantearenas/Documents/practicas/MODELOS/01_data/models_carveme/DIAMONT_2/ST00143dd.xml'))
-ST164_d = c.model(cobra.io.read_sbml_model('/home/abigaylmontantearenas/Documents/practicas/MODELOS/01_data/models_carveme/DIAMONT_2/ST00164dd.xml'))
 ST143_d.id ='Paenibacillus'
+ST164_d = c.model(cobra.io.read_sbml_model('/home/abigaylmontantearenas/Documents/practicas/MODELOS/01_data/models_carveme/DIAMONT_2/ST00164dd.xml'))
 ST164_d.id ='Bacillus'
 
 # set its initial biomass, 5e-6 gr at coordinate [0,0]
@@ -124,8 +21,6 @@ test_tube = c.layout()
 # add the models to the test tube
 test_tube.add_model(ST143_d)
 test_tube.add_model(ST164_d)
-
-
 
 test_tube.set_specific_metabolite("h2o_e", 100)
 test_tube.set_specific_metabolite("o2_e", 10)
@@ -203,10 +98,4 @@ comp_params.set_param('maxCycles', 240)
 comp_assay = c.comets(test_tube, comp_params)
 comp_assay.run()
 
-
-ax = comp_assay.total_biomass.plot(x = 'cycle', color = ['purple', 'green'])
-ax.set_ylabel("Biomass (gr.)")
-
-output_folder = 'graficas/CARVEME/DIMONT'
-output_path = os.path.join(output_folder, '143x164.png')
-plt.savefig(output_path)
+print(comp_assay.run_output)
